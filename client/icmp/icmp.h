ed487e62af5e83870396b84ac52cc6ff18a1564c
hajoon22 <fleshed-stroll84@icloud.com>
2026-09-13 14:38:55 +0900

    Add ICMP functions for echo and unreachable parsing

--- client/icmp/icmp.h
+++ client/icmp/icmp.h
@@ -1,6 +1,9 @@
 #ifndef ICMP_H
 #define ICMP_H
 
+#include <stdint.h>
+
 int send_icmp_echo(int s, uint32_t dst, uint16_t seq, uint8_t *data, size_t len);
+int parse_icmp_unreach(uint8_t *buf, size_t len, uint8_t **output);
 
 #endif
