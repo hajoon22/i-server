f9a3e2e21247d72701e14cb67fa7d905fac2708a
hajoon22 <fleshed-stroll84@icloud.com>
2026-09-13 14:45:17 +0900

    Modify parse_icmp_unreach to include sequence number

--- client/icmp/icmp.h
+++ client/icmp/icmp.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 
 int send_icmp_echo(int s, uint32_t dst, uint16_t seq, uint8_t *data, size_t len);
-int parse_icmp_unreach(uint8_t *buf, size_t len, uint8_t **output);
+int parse_icmp_unreach(uint8_t *buf, size_t len, uint16_t seq, uint8_t **output);
 
 #endif
