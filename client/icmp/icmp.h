b33b37f3aeab520fd3aa43acda48da87d8d1bf36
hajoon22 <hajoon@hajoon.me>
2026-09-14 02:07:04 +0900

    Change data pointer to const in send_icmp_echo

--- client/icmp/icmp.h
+++ client/icmp/icmp.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-int send_icmp_echo(int s, uint32_t dst, uint16_t seq, uint8_t *data, size_t len);
+int send_icmp_echo(int s, uint32_t dst, uint16_t seq, const uint8_t *data, size_t len);
 int parse_icmp_unreach(uint8_t *buf, size_t len, uint16_t seq, uint8_t **output);
 int parse_icmp_echo(uint8_t *buf, size_t len, uint16_t seq, uint8_t **output);
 
