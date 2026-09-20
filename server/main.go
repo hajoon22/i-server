476d4c8b943f9606deeeb48ad3d8a04c7c2567fa
hajoon22 <hajoon@hajoon.me>
2026-09-20 16:40:32 +0900

    Refactor main to init for config loading
    Refactored main function to init for configuration loading and added error handling.

--- server/main.go
+++ server/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"log"
+	"os"
+	"server/admin"
 	"server/client"
+	"server/config"
 )
 
-func main() {
+func init() {
+	cfg, err := config.ReadConfig("config.json")
+	if err != nil {
+		log.Printf("read config error: %s\r\n", err.Error())
+		os.Exit(-1)
+	}
+
 	go client.Maintain()
+	go admin.ListenWeb(cfg)
+}
+
+func main() {
 	err := client.Listen()
 	if err != nil {
 		log.Printf("client error: %s\n", err.Error())
