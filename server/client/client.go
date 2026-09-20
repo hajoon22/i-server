7bddb4f4060792ef244eb51f4e1cb95095c5569c
hajoon22 <hajoon@hajoon.me>
2026-09-20 16:42:05 +0900

    Add Fetch and message sending functionalities

--- server/client/client.go
+++ server/client/client.go
@@ -3,6 +3,10 @@ package client
 import (
 	"fmt"
 	"log"
+	"math/rand/v2"
+	"server/config"
+	"server/protocol"
+	"strings"
 	"sync"
 	"time"
 
@@ -18,10 +22,75 @@ type client struct {
 	LastSeen time.Time
 }
 
+const (
+	DefualtEchoID    = 2222
+	MessageEchoSeq   = 1111
+	KeepaliveEchoSeq = 2222
+	ClientTimeout    = 15 * time.Second
+)
+
 var (
 	mu      sync.Mutex
 	clients = map[string]*client{}
 )
+
+func Fetch() map[string]client {
+	mu.Lock()
+	defer mu.Unlock()
+
+	results := map[string]client{}
+	for id, c := range clients {
+		results[id] = *c
+	}
+
+	return results
+}
+
+func (c *client) sendMessage(cfg *config.Config, message string) {
+	dst := strings.Split(c.Addr, ".")
+	if len(dst) != 4 {
+		return
+	}
+
+	var refAddr string
+	for {
+		refAddr = cfg.Servers[rand.IntN(len(cfg.Servers))]
+		if refAddr == cfg.ServerAddr {
+			continue
+		}
+
+		break
+	}
+
+	ref := strings.Split(refAddr, ".")
+	if len(ref) != 4 {
+		return
+	}
+
+	inner, err := protocol.BuildECHO([4]string(dst), [4]string(ref), c.EchoID, MessageEchoSeq, message)
+	if err != nil {
+		return
+	}
+
+	if c.IsRewritten {
+		protocol.SendUnreach(c.Addr, inner)
+	} else {
+		protocol.SendIPIP(cfg.RelayAddr, inner)
+	}
+}
+
+func NewMessage(cfg *config.Config, message string) int {
+	mu.Lock()
+	defer mu.Unlock()
+
+	counter := 0
+	for _, c := range clients {
+		counter++
+		c.sendMessage(cfg, message)
+	}
+
+	return counter
+}
 
 func newClient(id string, echoID int, addr string) {
 	mu.Lock()
