fc4e12e5cb9e3d4fc7167ab7967e0c1ffeabd48b
hajoon22 <hajoon@hajoon.me>
2026-09-20 16:41:42 +0900

    Add configuration struct and ReadConfig function

---
+++ server/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Servers    []string `json:"servers"`
+	ServerAddr string   `json:"server_addr"`
+	RelayAddr  string   `json:"relay_addr"`
+	WebListen  string   `json:"web_listen"`
+	AdminKey   string   `json:"admin_key"`
+}
+
+func ReadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("read file error: %w", err)
+	}
+
+	var cfg Config
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("json unmarshal error: %w", err)
+	}
+
+	return &cfg, nil
+}
